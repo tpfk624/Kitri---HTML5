2421ad6f2f28846a037278c7b2532ee071b75f46
tpfk624 <tpfk624@naver.com>
2019-07-22 16:21:35 +0900

    0722 세라

--- membermvc/src/com/kitri/util/DBConnection.java
+++ membermvc/src/com/kitri/util/DBConnection.java
@@ -2,18 +2,33 @@ package com.kitri.util;
 
 import java.sql.*;
 
+import javax.naming.*;
+import javax.sql.DataSource;
+
 public class DBConnection {
 	
-	static {//생성자가 아니라 static블록 초기화를 쓰는 이유//첨에 한번만 읽어들이면 되니까
-		try {
-			Class.forName(SiteConstance.DB_DRIVER);
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}
-	}
+//	static {//생성자가 아니라 static블록 초기화를 쓰는 이유//첨에 한번만 읽어들이면 되니까
+//		try {
+//			Class.forName(SiteConstance.DB_DRIVER);
+//		} catch (ClassNotFoundException e) {
+//			e.printStackTrace();
+//		}
+//	}
+//	
+//	public static Connection makeConnection() throws SQLException{
+//		return DriverManager.getConnection(SiteConstance.DB_URL, SiteConstance.DB_USERNAME, SiteConstance.DB_PASSWORD);
+//	}
 	
 	public static Connection makeConnection() throws SQLException{
-		return DriverManager.getConnection(SiteConstance.DB_URL, SiteConstance.DB_USERNAME, SiteConstance.DB_PASSWORD);
+		try {
+			Context ictx = new InitialContext();
+			Context ctx = (Context) ictx.lookup("java:comp/env");
+			DataSource dataSource = (DataSource) ctx.lookup("jdbc/kitri");
+			return dataSource.getConnection();
+		} catch (NamingException e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
 }
 
